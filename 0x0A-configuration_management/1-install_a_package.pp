411a83820f9a3d941d84c10defc298bd18f055c3
Cheikyadam <urbaingande@gmail.com>
2024-02-23 11:52:42 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # Installing a package with puppet using pip3
-
+package { 'Werkzeug':
+  ensure   =>  '2.1.1',
+  provider =>  'pip3'
+  }
 package { 'Flask':
-  ensure   =>  'latest',
+  ensure   =>  '2.1.0',
   provider =>  'pip3'
   }
