534b3612a531084b695f9539b594dbc6ce23ad5e
Cheikyadam <urbaingande@gmail.com>
2024-02-25 18:28:57 -0800

    100_file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,9 @@
 # To config ~/.ssh/config
-include stdlib
 
 $my_path =  '/root/.ssh/config'
 
-file_line {'Turn off passwd auth':
-  ensure =>  present
-  path   =>  $my_path,
-  line   =>  'HostName 3.90.70.250'
-}
-
-file_line {'Turn off passwd auth':
-  ensure =>  present
-  path   =>  $my_path,
-  line   =>  'User ubuntu'
-}
-
-file_line {'Turn off passwd auth':
-  ensure =>  present
-  path   =>  $my_path,
-  line   =>  'PasswordAuthentication no'
-}
-
-file_line {'Declare identity file':
-  ensure =>  present
-  path   =>  $my_path,
-  line   =>  '~/.ssh/school'
+file {'config':
+  ensure  =>  present,
+  path    =>  $my_path,
+  content =>  'Host server\n	HostName 3.90.70.250\n	User ubuntu\n	IdentityFile ~/.ssh/school\n	PasswordAuthentication no'
 }
