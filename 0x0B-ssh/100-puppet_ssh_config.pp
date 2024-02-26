ac73ef8c3ebfc6ce0c5f9aa47e101b8ccd9925ff
Cheikyadam <urbaingande@gmail.com>
2024-02-26 10:32:24 -0800

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,17 @@
 # To config ~/.ssh/config
+include stdlib
+$my_path =  '/etc/ssh/ssh_config'
 
-$my_path =  ' /etc/ssh/ssh_config'
+file_line {'config_ident':
+  ensure =>  present,
+  path   =>  $my_path,
+  match  =>  '^*IdentityFile*rsa',
+  line   =>  '#   IdentityFile ~/.ssh/school'
+}
 
-file {'config':
-  ensure  =>  present,
-  path    =>  $my_path,
-  content => "Host server\n\tHostName 3.90.70.250\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no"
+file_line {'config_passw':
+  ensure =>  present,
+  path   =>  $my_path,
+  match  =>  '^*PasswordAuthentication',
+  line   =>  '#   PasswordAuthentication no'
 }
