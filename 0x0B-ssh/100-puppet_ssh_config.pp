7572de8d8895491934a7a36f0942b7a45d81cc07
Cheikyadam <urbaingande@gmail.com>
2024-02-25 18:17:20 -0800

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,6 +6,18 @@ $my_path =  '/root/.ssh/config'
 file_line {'Turn off passwd auth':
   ensure =>  present
   path   =>  $my_path,
+  line   =>  'HostName 3.90.70.250'
+}
+
+file_line {'Turn off passwd auth':
+  ensure =>  present
+  path   =>  $my_path,
+  line   =>  'User ubuntu'
+}
+
+file_line {'Turn off passwd auth':
+  ensure =>  present
+  path   =>  $my_path,
   line   =>  'PasswordAuthentication no'
 }
 
