7fcb751196851c3fb96a82d71cc8080d557c96fd
Cheikyadam <urbaingande@gmail.com>
2024-02-26 10:05:06 -0800

    100 upd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,5 +5,5 @@ $my_path =  '/root/.ssh/config'
 file {'config':
   ensure  =>  present,
   path    =>  $my_path,
-  content =>  'Host server\n	HostName 3.90.70.250\n	User ubuntu\n	IdentityFile ~/.ssh/school\n	PasswordAuthentication no'
+  content => "Host server\n\tHostName 3.90.70.250\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no"
 }
