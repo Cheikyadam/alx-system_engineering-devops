c43d57682d8e48576821004705265aa5cc8fa7c3
Cheikyadam <urbaingande@gmail.com>
2024-04-10 05:53:18 -0700

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,11 +10,11 @@ def top_ten(subreddit):
     agentChrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     agentChrome += 'AppleWebKit/537.36 (KHTML, like Gecko) '
     agentChrome += 'Chrome/97.0.4692.99 Safari/537.36'
-    headers = {
-                'User-Agent': agentChrome,
-                'Accept': 'application/json'
-                }
-
+    #headers = {
+     ##           'User-Agent': agentChrome,
+       #         'Accept': 'application/json'
+        #        }
+    headers = {'User-Agent': 'MyCustomApp/1.0 (https://www.example.com)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
@@ -23,7 +23,6 @@ def top_ten(subreddit):
             print(title)
     elif response.status_code == 301 or response.status_code == 302:
         print(None)
-        # return 0
     else:
         print(None)
-        # return 0
+        print(response.status_code)
