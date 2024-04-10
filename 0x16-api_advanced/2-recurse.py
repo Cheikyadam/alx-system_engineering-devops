64d6adc40349ada6a5928c682d7a6727385c919b
Cheikyadam <urbaingande@gmail.com>
2024-04-10 09:26:54 -0700

    2 test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """of subr"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     # headers = {'User-Agent':'python-requests/2.22.0'}
     # headers = {'User-Agent': 'MyCustomApp/1.0 (https://www.example.com)'}
     agentChrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
