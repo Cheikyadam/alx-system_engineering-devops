27022b4e823a07b5c2208ab862428d791c81b0b7
Cheikyadam <urbaingande@gmail.com>
2024-04-09 17:15:22 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
-        return response.get('data').get('subscribers')
+        return response.json().get('data').get('subscribers')
         # print("La requête a réussi.")
     elif response.status_code == 301 or response.status_code == 302:
         # print("Redirection détectée mais non suivie.")
