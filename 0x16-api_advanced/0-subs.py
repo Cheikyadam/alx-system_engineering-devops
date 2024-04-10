9527c34e5d3f3a2064cea33c6884bdd56b65ff32
Cheikyadam <urbaingande@gmail.com>
2024-04-10 03:16:31 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """of subr"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python-requests/2.22.0'}
-    response = requests.get(url, headers=headers)
+    #headers = {'User-Agent': 'python-requests/2.22.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    #response = requests.get(url, headers=headers)
     # allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
