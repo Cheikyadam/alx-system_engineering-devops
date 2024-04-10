5b393aa4b7b4cbb48c4b6ee74c67d346f2bcf5aa
Cheikyadam <urbaingande@gmail.com>
2024-04-10 02:25:04 -0700

    0 headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,15 @@ import requests
 def number_of_subscribers(subreddit):
     """of subr"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=False)
+    headers = {'User-Agent': 'python-requests/2.22.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         total = data['data']['subscribers']
         return total
     elif response.status_code == 301 or response.status_code == 302:
+        print(response.status_code)
         return 0
     else:
+        print(response.status_code)
         return 0
