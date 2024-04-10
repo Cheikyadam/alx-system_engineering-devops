3e0920655cf2d7601e148aa8f130d1e4c8eec5e9
Cheikyadam <urbaingande@gmail.com>
2024-04-10 06:05:29 -0700

    2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,14 @@ def top_ten(subreddit):
     """of subr"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     # headers = {'User-Agent':'python-requests/2.22.0'}
+    # headers = {'User-Agent': 'MyCustomApp/1.0 (https://www.example.com)'}
     agentChrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     agentChrome += 'AppleWebKit/537.36 (KHTML, like Gecko) '
     agentChrome += 'Chrome/97.0.4692.99 Safari/537.36'
-    #headers = {
-     ##           'User-Agent': agentChrome,
-       #         'Accept': 'application/json'
-        #        }
-    headers = {'User-Agent': 'MyCustomApp/1.0 (https://www.example.com)'}
+    headers = {
+               'User-Agent': agentChrome,
+               'Accept': 'application/json'
+               }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
