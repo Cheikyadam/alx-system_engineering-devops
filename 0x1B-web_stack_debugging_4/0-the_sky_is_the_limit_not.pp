e2aa3f2fa17cd92a40d9afea48fe51445e2700b5
Cheikyadam <urbaingande@gmail.com>
2024-04-18 18:57:45 -0700

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,11 @@ exec { 'limit_fix':
   command => "sed -i 's/^ULIMIT.*/ULIMIT=\"-n 15000\"/g' /etc/default/nginx",
   path    => ['/bin', '/usr/bin'],
 }
+
+
+exec { 'nginx_restart':
+  command     => '/usr/sbin/service nginx restart',
+  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+  refreshonly => true,
+  user        => 'root'
+}
